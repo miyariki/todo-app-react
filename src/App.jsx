9166da526a06e946d102830221d1b669fbc7c227
miyakita <40751806+miyariki@users.noreply.github.com>
2021-11-29 18:50:33 +0900

    props, stateの追加

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
+  const contentLedyStyle = {
+    color: "pink",
+    fontSize: "18px"
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
+    </>
+  );
+};
+
+export default App;
